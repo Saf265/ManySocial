b33a886788f3aa30189132b73894a231b41a0fb3
Saf265 <boucetasafwan@gmail.com>
2025-11-27 13:20:31 +0100

    user up

--- app/api/tools/generate-images/route.js
+++ app/api/tools/generate-images/route.js
@@ -1,6 +1,8 @@
 import { db } from "@/db/drizzle";
 import { ImagesGenerated } from "@/db/drizzle/schema";
+import { auth } from "@/lib/auth";
 import { nanoid } from "nanoid";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
@@ -29,15 +31,23 @@ export async function POST(request) {
 
   const randomId = nanoid();
 
+
+  console.log(data.data)
   // Extract image URLs from the response
   const imageUrls = data.data.map(img => img.url);
 
+  const headersList = await headers()
+  const session = await auth.api.getSession({
+    headers: headersList
+  })
+
   const [newImagesGenerated] = await db.insert(ImagesGenerated).values({
     id: randomId,
-    userId: "temp-user-id", // TODO: Get from session
+    userId: session?.user?.id ?? "none", // TODO: Get from session
     prompt: prompt,
     images: imageUrls
   }).returning();
+
 
   return NextResponse.json({ id: newImagesGenerated.id }, { status: 200 });
 }
